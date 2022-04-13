c2a4e43b2db2fdb67c3eaed607be5f0eef87c9f2
Andrew <atklenner@gmail.com>
2022-04-12 20:14:45 -0700

    Add mongodb

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://admin:password@172.28.0.2:27017/?authSource=admin")
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi there</h2>");
