3d3a8b6e4360a7a476af19f262af7d078cee51b9
Andrew <atklenner@gmail.com>
2022-04-12 22:36:19 -0700

    Add basic CRUD app

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
+const postRouter = require("./routes/postRoutes");
 
 const app = express();
 
+// should probably include logic that retries the mongo connection in case Docker doesn't start it up in time
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose
-  .connect("mongodb://admin:password@172.28.0.2:27017/?authSource=admin")
+  .connect(mongoURL)
   .then(() => console.log("connected to database"))
   .catch((err) => console.error(err));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi there</h2>");
 });
+
+app.use("/posts", postRouter);
 
 const port = process.env.port || 3000;
 
