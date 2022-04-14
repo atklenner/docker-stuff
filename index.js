9c38320e5e3f1ff75085721a8aa2d2a9924d5a9f
Andrew <atklenner@gmail.com>
2022-04-13 19:23:54 -0700

    Finish express app

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require("express");
-const mongoose = require("mongoose");
 const {
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
+  REDIS_URL,
+  REDIS_PORT,
+  SESSION_SECRET,
 } = require("./config/config");
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
+let RedisStore = require("connect-redis")(session);
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -17,13 +28,28 @@ mongoose
   .then(() => console.log("connected to database"))
   .catch((err) => console.error(err));
 
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUnitialized: false,
+      httpOnly: true,
+      maxAge: 30000000,
+    },
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi there</h2>");
 });
 
-app.use("/posts", postRouter);
+app.use("/api/v1/posts", postRouter);
+
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.port || 3000;
 
